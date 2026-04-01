882799a2b167dd82297d65e515f81fe2ca044fa2
minasmithp-star <mina.smith.p@gmail.com>
2026-04-01 14:52:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -561,6 +561,7 @@ function pomAnimatePhaseChange() {
 }
 
 
+function blendHex(hex, t) {
   const r = parseInt(hex.slice(1,3),16);
   const g = parseInt(hex.slice(3,5),16);
   const b = parseInt(hex.slice(5,7),16);
@@ -571,4 +572,4 @@ function pomAnimatePhaseChange() {
   return `rgb(${rr},${gg},${bb})`;
 }
 
-init();
+document.addEventListener('DOMContentLoaded', init);
